a5437ff687f94bcf73efa3c9cf243b5237929e62
KillirOficialxd <pproal2000@gmail.com>
2025-05-31 13:53:12 -0500

    DarkBallAdded

---
+++ Assets/Scripts/Enemy/DarkBall/DarkBall.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DarkBall : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
