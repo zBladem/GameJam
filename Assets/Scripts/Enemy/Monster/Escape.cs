a3cbba8c7f145f6fa5a0c9072dfd356383d51f6d
KillirOficialxd <pproal2000@gmail.com>
2025-05-31 07:20:48 -0500

    MonsterEscapeScript

---
+++ Assets/Scripts/Enemy/Monster/Escape.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Escape : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField] float speed;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb2d.linearVelocity = new Vector2(0, -1) * speed;
+    }
+}
