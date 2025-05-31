0eff3ece800038fdc06e368c37a778bf5e5e0440
KillirOficialxd <pproal2000@gmail.com>
2025-05-31 17:23:52 -0500

    Jumpscare & Gameover

--- Assets/Scripts/Enemy/Monster/Chase.cs
+++ Assets/Scripts/Enemy/Monster/Chase.cs
@@ -4,17 +4,31 @@ public class Chase : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float speed;
+    SpriteRenderer spr;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        if (GameObject.FindWithTag("Player").GetComponent<Health>().currentHP() <= 0)
+        switch (GameObject.FindWithTag("Player").GetComponent<Health>().currentHP())
         {
-            rb2d.linearVelocity = new Vector2(0, 1) * speed;
+            case 0:
+                rb2d.linearVelocity = new Vector2(0, 1) * speed;
+                spr.color = new Color(1, 1, 1, 1);
+                break;
+            case 1:
+                spr.color = new Color(1, 1, 1, 0.5f);
+                break;
+            case 2:
+                spr.color = new Color(1, 1, 1, 0.25f);
+                break;
+            case 3:
+                spr.color = new Color(1, 1, 1, 0);
+                break;
         }
     }
 }
